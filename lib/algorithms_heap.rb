5cb82eb1204aad76c6abfcca045b8f89c3470fcd
Lia Skalkos <hello@liaskalkos.net>
2015-08-27 17:17:04 -0400

    Complete

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,19 @@
 # code your solution here
+class MinHeap
+  attr_accessor :data, :root
+
+  def initialize
+    @data = []
+  end
+
+  def push(data)
+    self.data << data
+    self.root = self.data.min
+  end
+
+  def pop
+    value = self.data.pop
+    self.root = self.data.min
+    value
+  end
+end
